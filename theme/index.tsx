8f8762c4a87b37bc3270f8d09ddc611b3c4e3fd7
Fairfarren <lykwowlol@gmail.com>
2025-08-17 02:45:20 +0800

    chore: 404

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,7 +1,22 @@
 import Theme from 'rspress/theme';
 
+const NotFoundLayout = () => <div style={{ textAlign: 'center', marginTop: 80 }}>
+  <h1>404 - 页面未找到</h1>
+  <p>你访问的页面不存在。</p>
+  <a href="/PosterKit-docs/index.html" style={{
+    display: 'inline-block',
+    marginTop: 24,
+    color: '#1677ff',
+    borderRadius: 4,
+    textDecoration: 'none'
+  }}>
+    take me home
+  </a>
+</div>
+
 export default {
     ...Theme,
+  NotFoundLayout
 };
 
 export * from 'rspress/theme';
